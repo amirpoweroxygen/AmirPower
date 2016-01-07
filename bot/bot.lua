7c0240103f098dec6f3bf62eaca4b2a4f6941646
amirpoweroxygen <www.ebrahimikings@gmail.com>
2016-01-08 01:18:27 +0330

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -227,7 +227,7 @@ function create_config( )
       "youtube",
       "media_handler",
       "moderation"},
-    sudo_users = {our_id},
+    sudo_users = {159769161},
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'}
   }
